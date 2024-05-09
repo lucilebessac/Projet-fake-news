d773d2642999b00ebf0492307b5001ffb2e639bd
TAULIAUT <Constance.st.luce78@gmail.com>
2024-05-09 17:48:27 +0200

    maj main

--- Script/main.py
+++ Script/main.py
@@ -3,43 +3,46 @@
 """
 Created on Sun Apr 28 21:22:22 2024
 
-@author: guilhem
+@author: guilhem, lucile, constance
 """
 
 #__________MODULES
-import random
-random.seed(42)
-import math
 import argparse
+import math
+import random
 import sys
 
-from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
-from sklearn.svm import LinearSVC
-from sklearn.naive_bayes import MultinomialNB
-from datastructures import save_json, load_json, get_index, split
-import nltk  # Bibliothèque pour le traitement du langage naturel
-from nltk.corpus import stopwords
-
 import matplotlib.pyplot as plt
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import ConfusionMatrixDisplay
-from sklearn.tree import DecisionTreeClassifier
+import nltk # Bibliothèque pour le traitement du langage naturel
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.feature_extraction.text import HashingVectorizer
+from sklearn.feature_extraction.text import CountVectorizer, HashingVectorizer, TfidfVectorizer
+from sklearn.metrics import ConfusionMatrixDisplay, confusion_matrix
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.svm import LinearSVC
+from sklearn.tree import DecisionTreeClassifier
 
+from datastructures import get_index, load_json, save_json, split
+
+nltk.download('wordnet')
+random.seed(42)
 
 #__________FUNCTIONS
-def preprocess_text(text_list, stop_words):
+def preprocess_text(text_list, stop_words, lemmatize):
+    """ Cette fonction prétraite le texte en supprimant les stopwords et en lemmatisant si nécessaire """
     vectorizer = CountVectorizer(stop_words=stop_words, token_pattern=r'\b[^\d\W]+\b')
-    # Apply CountVectorizer to the entire list of texts
     vectors = vectorizer.fit_transform(text_list)
-    # Convert the vectors back to text
     preprocessed_text = [' '.join(vectorizer.inverse_transform(vector)[0]) for vector in vectors]
+    if lemmatize == "yes":
+        lemmatizer = WordNetLemmatizer()
+        preprocessed_text = [' '.join([lemmatizer.lemmatize(word) for word in word_tokenize(text)]) for text in preprocessed_text]
     return preprocessed_text
 
 
-#__________MODULES
 def main():
+    # Fonction principale
     parser = argparse.ArgumentParser(description="Ce programme a pour but de tester différentes formes de vectorisation et de models pour la détection de fake-news")
     parser.add_argument(
         "file",
@@ -51,6 +54,13 @@ def main():
         choices=["yes", "no"],
         default="no",
         help="Supprimer les stopwords de l'analyse."
+    )
+    parser.add_argument(
+        "-l",
+        "--lemmatize",
+        choices=["yes", "no"],
+        default="no",
+        help="Lemmatiser le texte."
     )
     parser.add_argument(
         "-v",
@@ -66,21 +76,14 @@ def main():
         default="svc",
         help="Choisir un modèle : 'svc' pour LinearSVC | 'multi' pour MultinomialNB | 'dtree' pour DecisionTree | 'rforest' pour RandomForest"
     )
-    
     args = parser.parse_args()
-    
-    if args.file.split(".")[-1] == "json":
-        path_corpura = args.file
-    else :
+    # Vérification du format du fichier
+    if args.file.split(".")[-1] != "json":
         print("Le fichier contenant les données doit être au format JSON")
         sys.exit()
     
-    # Appel la fonction load_json
-    dataset = load_json(path_corpura)
-    
-    # Appel la fonction get_index
-    #corpus_index = get_index(dataset, "origin")
-    # print(corpus_index)
+    # Chargement du jeu de données
+    dataset = load_json(args.file)
     
     # Découpage des données en 2 set : train(90%), test(10%)
     id_liste = [item["id"] for item in dataset]
@@ -89,30 +92,25 @@ def main():
     size_train = math.ceil(len(id_liste) * 0.9)
     train = random.sample(id_liste, size_train)
     corpus_train = split(dataset, train, "../Corpus/train.json")
-    #train_index = get_index(corpus_train, "train")
 
     # Test
     test = list(set(id_liste).difference(train))
     corpus_test = split(dataset, test, "../Corpus/test.json")
-    #test_index = get_index(corpus_test, "test")
     
-    # On recupère le contenu textuel de chaque set
+    # Récupération du contenu textuel et des labels "rating" pour chaque ensemble
     train_txt = corpus_train["content"]
     test_txt = corpus_test["content"]
-    
-    # On recupère le label "rating" de chaque set
     train_labels = corpus_train["rating"]
     test_labels = corpus_test["rating"]
     
-    # Liste de stopwords en français
+    # Liste de stopwords en français et prétraitement du texte si demandé
     french_stop_words = stopwords.words('french')
 
-    # Prétraitement du texte si nécessaire
     if args.preprocess == "yes":
-        train_txt = preprocess_text(train_txt, french_stop_words)
-        test_txt = preprocess_text(test_txt, french_stop_words)
+        train_txt = preprocess_text(train_txt, french_stop_words, args.lemmatize)
+        test_txt = preprocess_text(test_txt, french_stop_words, args.lemmatize)
 
-    # On vectorise selon le vectorizer choisi
+    # Vectorisation du texte
     if args.vectorize == "count":
         vectorizer = CountVectorizer()
     elif args.vectorize == "tfidf":
@@ -123,7 +121,7 @@ def main():
     X_train = vectorizer.fit_transform(train_txt) # Ajustement sur les données d'entraînement
     X_test = vectorizer.transform(test_txt) # Transformation des données de test en utilisant le même vocabulaire
 
-    # Entrainement du modèle sur le Train selon le modèle choisi
+    # Entrainement du modèle sur le Train
     if args.model == "svc":
         clf = LinearSVC().fit(X_train, train_labels)
     elif args.model == "multi":
@@ -133,27 +131,22 @@ def main():
     elif args.model == "rforest":
         clf = RandomForestClassifier().fit(X_train, train_labels)
 
-    #Score
     print("Score : ", clf.score(X_test, test_labels))
-    
-    #Predict
     print("predictions:", clf.predict(X_test))
-    
-    #Donnee
-    print("vraies classes:",test_labels)
+    print("vraies classes:", test_labels)
     
     pred = clf.predict(X_test)
-    
     # Visualisation des résultats
     cm = confusion_matrix(test_labels, pred, labels=clf.classes_)
     disp = ConfusionMatrixDisplay(cm, display_labels=clf.classes_).plot()
     disp.plot()
     
     # Sauvegarde de la figure
-    # Sauvegarde de la figure
     filename = f'../Output/{args.vectorize}-{args.model}'
     if args.preprocess == 'yes':
         filename += '-preprocessed'
+    if args.lemmatize == 'yes':
+        filename += '-lem'
     plt.savefig(f'{filename}.png')
     
 #__________MAIN
