223e1d78a0f6282e3e7c9768209f5200aa6999fa
TAULIAUT <Constance.st.luce78@gmail.com>
2024-05-09 14:21:12 +0200

    ajouts fichiers

--- Script/main.py
+++ Script/main.py
@@ -19,11 +19,23 @@ from sklearn.naive_bayes import MultinomialNB
 from datastructures import save_json, load_json, get_index, split
 import nltk  # Bibliothèque pour le traitement du langage naturel
 from nltk.corpus import stopwords
+
 import matplotlib.pyplot as plt
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import ConfusionMatrixDisplay
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_extraction.text import HashingVectorizer
+
 
 #__________FUNCTIONS
+def preprocess_text(text_list, stop_words):
+    vectorizer = CountVectorizer(stop_words=stop_words, token_pattern=r'\b[^\d\W]+\b')
+    # Apply CountVectorizer to the entire list of texts
+    vectors = vectorizer.fit_transform(text_list)
+    # Convert the vectors back to text
+    preprocessed_text = [' '.join(vectorizer.inverse_transform(vector)[0]) for vector in vectors]
+    return preprocessed_text
 
 
 #__________MODULES
@@ -38,21 +50,21 @@ def main():
         "--preprocess",
         choices=["yes", "no"],
         default="no",
-        help="Supprimer les stopwords de l'analyse et les ponctuations."
+        help="Supprimer les stopwords de l'analyse."
     )
     parser.add_argument(
         "-v",
         "--vectorize",
-        choices=["count", "tfidf"],
+        choices=["count", "tfidf", "hash"],
         default="count",
-        help="Choisir le vectorizer -> 'count' : CountVectorizer | 'tfidf' : TfidfVectorizer"
+        help="Choisir le vectorizer : 'count' pour CountVectorizer | 'tfidf' pour TfidfVectorizer | 'hash' pour HashingVectorizer"
     )
     parser.add_argument(
         "-m",
         "--model",
-        choices=["svc", "multi"],
+        choices=["svc", "multi", "dtree", "rforest"],
         default="svc",
-        help="Choisir un model : 'svc' : LinearSVC | 'multi' MultinomialNB | "
+        help="Choisir un modèle : 'svc' pour LinearSVC | 'multi' pour MultinomialNB | 'dtree' pour DecisionTree | 'rforest' pour RandomForest"
     )
     
     args = parser.parse_args()
@@ -67,7 +79,7 @@ def main():
     dataset = load_json(path_corpura)
     
     # Appel la fonction get_index
-    corpus_index = get_index(dataset, "origin")
+    #corpus_index = get_index(dataset, "origin")
     # print(corpus_index)
     
     # Découpage des données en 2 set : train(90%), test(10%)
@@ -77,12 +89,12 @@ def main():
     size_train = math.ceil(len(id_liste) * 0.9)
     train = random.sample(id_liste, size_train)
     corpus_train = split(dataset, train, "../Corpus/train.json")
-    train_index = get_index(corpus_train, "train")
+    #train_index = get_index(corpus_train, "train")
 
     # Test
     test = list(set(id_liste).difference(train))
     corpus_test = split(dataset, test, "../Corpus/test.json")
-    test_index = get_index(corpus_test, "test")
+    #test_index = get_index(corpus_test, "test")
     
     # On recupère le contenu textuel de chaque set
     train_txt = corpus_train["content"]
@@ -94,29 +106,32 @@ def main():
     
     # Liste de stopwords en français
     french_stop_words = stopwords.words('french')
-    count_vectorizer = CountVectorizer(stop_words=french_stop_words)
-    tfidf_vectorizer = TfidfVectorizer(stop_words=french_stop_words, max_df=0.7)
-    
+
+    # Prétraitement du texte si nécessaire
+    if args.preprocess == "yes":
+        train_txt = preprocess_text(train_txt, french_stop_words)
+        test_txt = preprocess_text(test_txt, french_stop_words)
+
     # On vectorise selon le vectorizer choisi
     if args.vectorize == "count":
-        if args.preprocess == "yes":
-            vectorizer = CountVectorizer(stop_words=french_stop_words, token_pattern=r'\b[^\d\W]+\b')
-        else:
-            vectorizer = CountVectorizer()
-        X_train = vectorizer.fit_transform(train_txt) # Ajustement sur les données d'entraînement
-        X_test = vectorizer.transform(test_txt) # Transformation des données de test en utilisant le même vocabulaire
+        vectorizer = CountVectorizer()
     elif args.vectorize == "tfidf":
-        if args.preprocess == "yes":
-            vectorizer = TfidfVectorizer(stop_words=french_stop_words, token_pattern=r'\b[^\d\W]+\b')
-        else:
-            vectorizer = TfidfVectorizer()
-        X_train = vectorizer.fit_transform(train_txt)
-        X_test = vectorizer.transform(test_txt)        
+        vectorizer = TfidfVectorizer()
+    elif args.vectorize == "hash":
+        vectorizer = HashingVectorizer()
+
+    X_train = vectorizer.fit_transform(train_txt) # Ajustement sur les données d'entraînement
+    X_test = vectorizer.transform(test_txt) # Transformation des données de test en utilisant le même vocabulaire
+
     # Entrainement du modèle sur le Train selon le modèle choisi
     if args.model == "svc":
         clf = LinearSVC().fit(X_train, train_labels)
     elif args.model == "multi":
         clf = MultinomialNB().fit(X_train, train_labels)
+    elif args.model == "dtree":
+        clf = DecisionTreeClassifier().fit(X_train, train_labels)
+    elif args.model == "rforest":
+        clf = RandomForestClassifier().fit(X_train, train_labels)
 
     #Score
     print("Score : ", clf.score(X_test, test_labels))
@@ -135,12 +150,12 @@ def main():
     disp.plot()
     
     # Sauvegarde de la figure
+    # Sauvegarde de la figure
     filename = f'../Output/{args.vectorize}-{args.model}'
-    if args.preprocess == 'no':
-        filename += '-sw'   # si no, il y a des stopwords donc -sw dans le nom fichier
-    plt.savefig(filename)
-    #plt.savefig(f'../Output/{args.vectorize}-{args.model}.png')
-
+    if args.preprocess == 'yes':
+        filename += '-preprocessed'
+    plt.savefig(f'{filename}.png')
+    
 #__________MAIN
 if __name__ == "__main__":
     main()
